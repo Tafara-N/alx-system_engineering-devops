6c432a008973df66996eae2a14a4f75451792aad
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-22 04:23:33 +0200

    A Puppet script that installs Flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-# A Puppet script to install flask from pip3
+# A Puppet script to install flask from pip3 and ensures the version is '2.1.0'
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
