a5296c70c6695002d48fdd20f620f34e68866883
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-22 10:44:29 +0200

    A Puppet script that creates a file at /tmp/school with specific attributes.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# A Puppet script that creates a file at /tmp/school with specific attributes
+
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
