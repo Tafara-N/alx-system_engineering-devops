e6520d96bfed1e71830a08322d1a95ee07d95b10
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-07 20:08:07 +0200

    Script queries Reddit API and returns a list containing the titles of all hotarticles for a given subreddit.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,16 +12,16 @@ after = None
 
 def recurse(subreddit, hot_list=[]):
     """
-    Returning top ten post titles recursively
+    Returning the top ten post titles recursively
     """
 
     global after
 
-    user_agent = {'User-Agent': 'api_advanced-project'}
+    user_agent = {"User-Agent": "api_advanced-project"}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+    parameters = {"after": after}
+    results = requests.get(url, params=parameters,
+                           headers=user_agent, allow_redirects=False)
 
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
@@ -29,6 +29,7 @@ def recurse(subreddit, hot_list=[]):
             after = after_data
             recurse(subreddit, hot_list)
         all_titles = results.json().get("data").get("children")
+
         for title_ in all_titles:
             hot_list.append(title_.get("data").get("title"))
         return hot_list
