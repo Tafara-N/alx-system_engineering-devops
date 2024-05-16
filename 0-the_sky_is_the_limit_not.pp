ff3b7f70b571718f80a7cbe42555c4418208a487
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-05-16 15:32:44 +0000

    Sky is the limit, let's bring that limit higher.

---
+++ 0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# Fixing Nginx so it accepts and serves more requests
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
+
